0495f71576603387999c74ad4f2691b2afec4b62
Forrest Jacobs <forrestjacobs@gmail.com>
2023-06-26 22:53:45 -0400

    Fix rollup build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,9 +2,18 @@ import typescript from "@rollup/plugin-typescript";
 
 export default {
   input: "src/index.ts",
+  external: ["pellicola"],
   output: {
     file: "out/bundle.cjs",
     format: "cjs",
   },
-  plugins: [typescript()],
+  plugins: [
+    typescript({
+      tsconfig: false,
+      target: "ES2020",
+      module: "ESNext",
+      lib: ["ES2021", "DOM", "DOM.Iterable"],
+      allowSyntheticDefaultImports: true,
+    }),
+  ],
 };
